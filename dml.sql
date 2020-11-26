940ac9e575f7a4bd0e82439d84804374968ed5e8
Jayden Z <41285962+GodkingMedicus@users.noreply.github.com>
2020-11-26 16:27:52 +1100

    more stuff

--- dml.sql
+++ dml.sql
@@ -1,3 +1,8 @@
 INSERT INTO TeamMember
     (Email,Password,Role,Authorized)
 Values('test', 'Password', 'Authorized', 1)
+
+INSERT INTO GAME
+    (GameDate, Venue)
+Values("Knox" "2020-11-07T11:59:00"),
+("Box Hill" "2020-11-07T11:59:00")
